208329bd8a353cdaabdee95347a46c6a42f414c4
CelineSelke1 <celineselke@outlook.com>
2025-04-20 10:39:51 -0400

    add starter code

---
+++ src/FilteredList.jsx
@@ -0,0 +1,36 @@
+import React, {Component} from 'react'; import
+
+List from './List';
+import { render } from '@testing-library/react';
+
+class FilteredList extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            search: ""
+        };
+    }
+
+    onSearch = (event) => {
+        this.setState({search: event.target.value.toLowerCase()});
+    }
+
+    filterItem = (item) => {
+        return item.name.toLowerCase().search(this.state.search) !== -1;
+    }
+
+    render(){
+        return(
+
+            <div className='filter-list'>
+                <h1>Produce Search</h1>
+                <input type="text" placeholder='Search' onChange={this.onSearch}/>
+                <List items={this.props.items.filter(this.filterItem)}/>
+            </div>
+
+        );
+    }
+
+}
+
+export default FilteredList;
